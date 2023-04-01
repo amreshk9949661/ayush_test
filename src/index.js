582699059e0f7017623cb3706d4d4fc9ed46669a
amreshk9949661 <amreshk994@gmail.com>
2023-04-01 14:05:11 +0530

    initialize project

---
+++ src/index.js
@@ -0,0 +1,2 @@
+module.exports.Token = require('./services/token.service');
+module.exports.User = require('./services/user.service');
---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    auth:require('./auth.service'),
+    email:require('./email.service'),
+    token:require('./token.service'),
+    user:require('./user.service')
+}
